f20f8730d6b4acc710077e42cea775cc490a6ccf
yoonc01 <yoonc01@hotmail.com>
2026-01-09 15:31:52 +0900

    feat: init prisma module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
 import { AuthModule } from "./auth/auth.module";
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
