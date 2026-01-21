96fa8eb5013f47ba60ebdcc16a4d0bc22e171892
yoonc01 <yoonc01@hotmail.com>
2026-01-21 22:27:39 +0900

    feat: remove Api test

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,11 +1,5 @@
-import { Controller, Get, Req, UseGuards } from "@nestjs/common";
+import { Controller, Get } from "@nestjs/common";
 import { AppService } from "./app.service";
-import { AccessTokenGuard } from "./auth/guards/access-token.guard";
-import { Request } from "express";
-import { ApiBearerAuth, ApiOkResponse } from "@nestjs/swagger";
-import { UserTestResponseDto } from "./app/dto/user-test-response.dto";
-
-type RequestWithUser = Request & { user: Express.User };
 
 @Controller()
 export class AppController {
@@ -16,11 +10,4 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Get("/user-test")
-  @UseGuards(AccessTokenGuard)
-  @ApiBearerAuth("access-token")
-  @ApiOkResponse({ type: UserTestResponseDto })
-  testUser(@Req() req: RequestWithUser): UserTestResponseDto {
-    return { message: `user email : ${req.user.email}` };
-  }
 }
